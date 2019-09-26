397a2236e1d428e23753e6d9661a466b1ddfe1ea
Sunny Beatteay <sbeatteay@digitalocean.com>
2019-09-26 11:34:11 -0400

    update deploy script

--- ci/scripts/deploy.sh
+++ ci/scripts/deploy.sh
@@ -2,13 +2,11 @@
 
 echo "Deploying app"
 
-DOCKER_HOST="ssh://$SSH_USER@$SSH_IP"
-
 ssh-add "$SSH_KEY_PATH"
 
-docker pull sunlib/wonderchat:latest
-docker stop wonderchat
-docker rm wonderchat
-docker run --name wonderchat -d -p 80:3000 sunlib/wonderchat:latest deploy/bin/start-node
+ssh "$SSH_USER@$SSH_IP" docker pull sunlib/wonderchat:latest
+ssh "$SSH_USER@$SSH_IP" docker stop wonderchat
+ssh "$SSH_USER@$SSH_IP" docker rm wonderchat
+ssh "$SSH_USER@$SSH_IP" docker run --name wonderchat -d -p 80:3000 sunlib/wonderchat:latest deploy/bin/start-node
 
 echo "Finished deploying app"
