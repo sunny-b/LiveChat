803c8a2c95994e0bcf289b8857fd0bb46da85e3d
Sunny Beatteay <sbeatteay@digitalocean.com>
2019-09-26 13:58:56 -0400

    fix npm build script

--- ci/scripts/deploy.sh
+++ ci/scripts/deploy.sh
@@ -7,6 +7,6 @@ ssh-add "$SSH_KEY_PATH"
 ssh -o "StrictHostKeyChecking no" "$SSH_USER@$SSH_IP" docker pull sunlib/wonderchat:latest
 ssh -o "StrictHostKeyChecking no" "$SSH_USER@$SSH_IP" docker stop wonderchat
 ssh -o "StrictHostKeyChecking no" "$SSH_USER@$SSH_IP" docker rm wonderchat
-ssh -o "StrictHostKeyChecking no" "$SSH_USER@$SSH_IP" docker run --name wonderchat -d -p 80:3000 sunlib/wonderchat:latest deploy/bin/start-node
+ssh -o "StrictHostKeyChecking no" "$SSH_USER@$SSH_IP" docker run --name wonderchat -d -p 3000:3000 sunlib/wonderchat:latest deploy/bin/start-node
 
 echo "Finished deploying app"
