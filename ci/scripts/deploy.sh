ce6073f2b950269ba4804eabea37941037eab40c
Sunny Beatteay <sbeatteay@digitalocean.com>
2019-09-26 11:48:05 -0400

    remove host check

--- ci/scripts/deploy.sh
+++ ci/scripts/deploy.sh
@@ -4,9 +4,9 @@ echo "Deploying app"
 
 ssh-add "$SSH_KEY_PATH"
 
-ssh "$SSH_USER@$SSH_IP" docker pull sunlib/wonderchat:latest
-ssh "$SSH_USER@$SSH_IP" docker stop wonderchat
-ssh "$SSH_USER@$SSH_IP" docker rm wonderchat
-ssh "$SSH_USER@$SSH_IP" docker run --name wonderchat -d -p 80:3000 sunlib/wonderchat:latest deploy/bin/start-node
+ssh -o "StrictHostKeyChecking no" "$SSH_USER@$SSH_IP" docker pull sunlib/wonderchat:latest
+ssh -o "StrictHostKeyChecking no" "$SSH_USER@$SSH_IP" docker stop wonderchat
+ssh -o "StrictHostKeyChecking no" "$SSH_USER@$SSH_IP" docker rm wonderchat
+ssh -o "StrictHostKeyChecking no" "$SSH_USER@$SSH_IP" docker run --name wonderchat -d -p 80:3000 sunlib/wonderchat:latest deploy/bin/start-node
 
 echo "Finished deploying app"
